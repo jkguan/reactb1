74c338db396fae14f2f13d520ca403ce12da082c
jkguan <jacksonguan89@gmail.com>
2020-10-11 07:21:52 +0700

    CapNhatB3

--- src/BaiTapGioHang/BTGioHang.js
+++ src/BaiTapGioHang/BTGioHang.js
@@ -95,9 +95,9 @@ export default class BTGioHang extends Component {
               className="modal fade"
               id="modelId"
               tabIndex={-1}
-              role="dialog"
               aria-labelledby="modelTitleId"
               aria-hidden="true"
+              style={{ display: "none" }}
             >
               <div
                 className="modal-dialog"
@@ -128,7 +128,23 @@ export default class BTGioHang extends Component {
                           <th>thành tiền</th>
                         </tr>
                       </thead>
-                      <tbody />
+                      <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td>Iphone XS Max</td>
+                          <td>
+                            <img src="./img/applephone.jpg" width={50} alt />
+                          </td>
+                          <td>
+                            <button>-</button>5<button>+</button>
+                          </td>
+                          <td>27000000</td>
+                          <td>135000000</td>
+                          <td>
+                            <button className="btn btn-danger">Delete</button>
+                          </td>
+                        </tr>
+                      </tbody>
                     </table>
                   </div>
                   <div className="modal-footer">
